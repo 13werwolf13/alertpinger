394cd98796a0cf2593bf607833fcc2f472ec1350
13werwolf13 <13werwolf13@mail.ru>
2016-04-27 13:51:16 +0600

    Update alertpinger.py

--- alertpinger.py
+++ alertpinger.py
@@ -9,16 +9,16 @@ def timetoping():
     pinger.startping()
 
     if pinger.aaa == 1:
-        bot.send_message(constants.chatid, "10.0.1.10 is down")
+        bot.send_message(constants.chatid, "srv1 is down")
 
     if pinger.bbb == 1:
-        bot.send_message(constants.chatid, "10.0.1.11 is down")
+        bot.send_message(constants.chatid, "srv2 is down")
 
     if pinger.ccc == 1:
-        bot.send_message(constants.chatid, "10.0.1.12 is down")
+        bot.send_message(constants.chatid, "srv3 is down")
 
     if pinger.ddd == 1:
-        bot.send_message(constants.chatid, "10.0.1.16 is down")
+        bot.send_message(constants.chatid, "srv4 is down")
 
     time.sleep(5);
 
